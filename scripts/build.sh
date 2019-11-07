814fdf2e78591a5cd26bbd7bb82cf11e3f944fdd
Dmytro Tkachuk <dmitry.tkk@gmail.com>
2019-11-07 17:49:13 +0200

    SKALE-1733 add travis ci integration, add automated builds

---
+++ scripts/build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+
+NAME=transactions-manager
+REPO_NAME=skalelabshub/$NAME
+IMAGE_NAME=$REPO_NAME:$VERSION
+LATEST_IMAGE_NAME=$REPO_NAME:latest
+
+: "${VERSION?Need to set VERSION}"
+
+echo "Building $IMAGE_NAME..."
+docker build -t $IMAGE_NAME . || exit $?
+
+if [ "$RELEASE" = true ]
+then
+    docker tag $IMAGE_NAME $LATEST_IMAGE_NAME
+fi
+
+echo "========================================================================================="
+echo "Built $IMAGE_NAME"
---
+++ scripts/publish.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+NAME=transactions-manager
+REPO_NAME=skalelabshub/$NAME
+IMAGE_NAME=$REPO_NAME:$VERSION
+LATEST_IMAGE_NAME=$REPO_NAME:latest
+
+: "${USERNAME?Need to set USERNAME}"
+: "${PASSWORD?Need to set PASSWORD}"
+
+echo "$PASSWORD" | docker login --username $USERNAME --password-stdin
+docker push $IMAGE_NAME || exit $?
+if [ "$RELEASE" = true ]
+then
+    docker push $LATEST_IMAGE_NAME || exit $?
+fi
