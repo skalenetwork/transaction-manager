9ca6fa4a222eed424f05d167f14d0c01b8ba9e99
Dmytro Tkachuk <dmitry.tkk@gmail.com>
2020-04-26 23:51:38 +0300

    SKALE-2336 Add test stage to travis

---
+++ scripts/run-tests.sh
@@ -0,0 +1,19 @@
+
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+PROJECT_DIR=$(dirname $DIR)
+
+export SKALE_DIR_HOST=$PWD/tests/skale-data
+export ENDPOINT=http://localhost:8545
+
+export FLASK_APP_HOST=0.0.0.0
+export FLASK_APP_PORT=3008
+export FLASK_DEBUG_MODE=True
+export FLASK_SECRET_KEY=123
+
+export PK_FILE=$PROJECT_DIR/pk_file
+
+echo $ETH_PRIVATE_KEY > $TEST_PK_FILE
+
+py.test $PROJECT_DIR/tests/
--- scripts/run_tests.sh
+++
@@ -1,4 +0,0 @@
-
-
-
-export SKALE_DIR_HOST=$PWD/tests/skale-data
