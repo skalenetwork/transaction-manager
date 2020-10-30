043ac2a58c078248009747cc511cc2ac69dc39c7
badrogger <ivan@skalelabs.com>
2020-10-30 11:22:24 +0200

    SKALE-3350 Fix codacy in test scripts

--- scripts/cleanup-tests.sh
+++ scripts/cleanup-tests.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 docker-compose down
 echo 'WARNING the following command will be executed with sudo!'
 sudo rm -rf tests/data-volumes/*
--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -5,20 +5,20 @@ PROJECT_DIR=$(dirname $DIR)
 
 : "${SGX_WALLET_TAG?Need to set SGX_WALLET_TAG}"
 
-VOLUME_DIR=tests/data-volumes
-SKALE_DIR=$PWD/tests/data-volumes/skale-dir
-SGX_DIR=$PWD/tests/data-volumes/sgx-dir
-REDIS_DIR=$PWD/tests/data-volumes/redis-dir
+VOLUME_DIR="$PWD/tests/data-volumes"
+SKALE_DIR="$VOLUME_DIR/skale-dir"
+SGX_DIR="$VOLUME_DIR/sgx-dir"
+REDIS_DIR="$VOLUME_DIR/redis-dir"
 REDIS_DATA_DIR=$REDIS_DIR/redis-data
 REDIS_CONFIG_DIR=$REDIS_DIR/redis-config
 SKALE_NODE_DATA_DIR=$SKALE_DIR/node_data
 SKALE_LOG_DIR=$SKALE_NODE_DATA_DIR/log
 SKALE_CONTRACTS_INFO=$SKALE_DIR/contracts_info
 
-mkdir -p $SKALE_LOG_DIR
-mkdir -p $REDIS_CONFIG_DIR $REDIS_DATA_DIR
-mkdir -p $SGX_DIR
-cp tests/test-redis.conf $REDIS_CONFIG_DIR/redis.conf
+mkdir -p "$SKALE_LOG_DIR"
+mkdir -p "$REDIS_CONFIG_DIR" "$REDIS_DATA_DIR"
+mkdir -p "$SGX_DIR"
+cp tests/test-redis.conf "$REDIS_CONFIG_DIR/redis.conf"
 
 export ENDPOINT=http://127.0.0.1:8545
 export PYTHONPATH=$PROJECT_DIR
@@ -38,15 +38,9 @@ docker-compose build --no-cache && docker-compose up -d
 echo 'Wating for sgx initialization ...'
 sleep 50
 
-TEST_DATA_DIR='helper-scripts/contracts_data/manager.json'
-
 NODE_DATA_PATH=$SKALE_NODE_DATA_DIR \
 STDERR_LOG=True \
 TEST_ABI_FILEPATH=$TEST_ABI_FILEPATH \
     py.test \
     --cov-report term-missing --cov=. --capture no \
-    $PROJECT_DIR/tests/main_test.py
-
-# TEST_DATA_DIR=$SKALE_DIR_HOST SGX_WALLET_TAG=$SGX_WALLET_TAG docker-compose down
-# TEST_DATA_DIR=$SKALE_DIR_HOST SGX_WALLET_TAG=$SGX_WALLET_TAG docker-compose rm -f
-# rm -r $VOLUME_DIR/*
+    "$PROJECT_DIR/tests/main_test.py"
