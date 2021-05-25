999ce29f66561b1d0721205cb77341fe9f9266a1
badrogger <ivan@skalelabs.com>
2021-05-25 15:27:02 +0300

    SKALE-3350 Add wallet to resources

--- transaction_manager/resources.py
+++ transaction_manager/resources.py
@@ -1,10 +1,14 @@
 import redis
 
-from .config import ENDPOINT, REDIS_URI
+from .config import ENDPOINT, ETH_PRIVATE_KEY, REDIS_URI
 from skale.utils.web3_utils import init_web3  # type: ignore
+from skale.wallets import BaseWallet, Web3Wallet  # type: ignore
+from web3 import Web3
 
 # TODO: IVD check out options to configure
-cpool = redis.ConnectionPool.from_url(REDIS_URI)
-rs = redis.Redis(connection_pool=cpool)
+cpool: redis.ConnectionPool = redis.ConnectionPool.from_url(REDIS_URI)
+rs: redis.Redis = redis.Redis(connection_pool=cpool)
 # TODO: IVD check out options to configure
-w3 = init_web3(ENDPOINT)
+w3: Web3 = init_web3(ENDPOINT)
+
+wallet: BaseWallet = Web3Wallet(ETH_PRIVATE_KEY, w3)
