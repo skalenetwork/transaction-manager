423f891375a4df233f53819218987f17356c2ba8
badrogger <ivan@skalelabs.com>
2021-05-25 13:32:10 +0300

    SKALE-3350 Add main.py file

--- transaction_manager/main.py
+++ transaction_manager/main.py
@@ -1,7 +1,18 @@
+import logging
+from transaction_manager.eth import Eth
+from transaction_manager.processor import Processor
 from transaction_manager.txpool import TxPool
+from transaction_manager.wallet import init_wallet
+
+logger = logging.getLogger(__name__)
+
 
 def main() -> None:
-    pass
+    eth = Eth()
+    pool = TxPool()
+    wallet = init_wallet()
+    proc = Processor(eth, pool, wallet)
+    proc.run()
 
 
 if __name__ == '__main__':
