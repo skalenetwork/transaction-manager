babbf2f97d42dee851c857d862eb13d54eaf8983
badrogger <ivan@skalelabs.com>
2021-05-31 18:15:54 +0300

    SKALE-3350 Add sgx wallet support

--- transaction_manager/main.py
+++ transaction_manager/main.py
@@ -1,19 +1,29 @@
 import logging
+# import time
+
 from .eth import Eth
+from .log import init_logger
+# from .node import is_config_created
 from .processor import Processor
 from .txpool import TxPool
 from .wallet import init_wallet
-from .log import init_logger
 
 logger = logging.getLogger(__name__)
 
 
 def main() -> None:
     init_logger()
+
+    # while not is_config_created():
+    #     logger.info('Waiting for node config generation ...')
+    #     time.sleep(2)
+
+    logger.info('Initializing transaction manager ...')
     eth = Eth()
     pool = TxPool()
     wallet = init_wallet()
     proc = Processor(eth, pool, wallet)
+    logger.info('Starting transaction processor ...')
     proc.run()
 
 
--- transaction_manager/wallet.py
+++ transaction_manager/wallet.py
@@ -1,12 +1,16 @@
 import logging
+import os
 
 from skale.wallets import BaseWallet, SgxWallet, Web3Wallet  # type: ignore
 from web3 import Web3
 
-from .config import ETH_PRIVATE_KEY, SGX_URL
+from .config import ETH_PRIVATE_KEY, NODE_DATA_PATH, SGX_URL
+from .node import get_sgx_keyname
 from .resources import w3 as gw3
 
 logger = logging.getLogger(__name__)
+
+PATH_TO_SGX_CERT = os.path.join(NODE_DATA_PATH, 'sgx_cert')
 
 
 class WalletInitializationError(Exception):
@@ -14,10 +18,21 @@ class WalletInitializationError(Exception):
 
 
 def init_wallet(w3: Web3 = gw3) -> BaseWallet:
-    if SGX_URL:
-        return SgxWallet(SGX_URL, w3)
-    elif ETH_PRIVATE_KEY:
-        return Web3Wallet(ETH_PRIVATE_KEY, w3)
-    else:
+    wallet = None
+    if ETH_PRIVATE_KEY:
+        logger.info('Initializing web3 Wallet ...')
+        wallet = Web3Wallet(ETH_PRIVATE_KEY, w3)
+    elif SGX_URL:
+        logger.info('Initializing sgx wallet ...')
+        keyname = get_sgx_keyname()
+        wallet = SgxWallet(
+            SGX_URL,
+            w3,
+            key_name=keyname,
+            path_to_cert=PATH_TO_SGX_CERT
+        )
+    if not wallet:
         logger.warning('Both SGX_URL and ETH_PRIVATE_KEY was not provided')
         raise WalletInitializationError('Failed to initialize wallet')
+    logger.info(f'Wallet address {wallet.address}')
+    return wallet
