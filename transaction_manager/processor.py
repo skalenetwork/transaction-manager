49ee0b719ef7ec9bfe786743df79065136bb3c72
badrogger <ivan@skalelabs.com>
2021-05-31 18:10:43 +0300

    SKALE-3350 Fix defaults. Add logs

--- transaction_manager/processor.py
+++ transaction_manager/processor.py
@@ -1,15 +1,20 @@
 import logging
 import time
+from typing import Dict
 
 from skale.wallets import BaseWallet  # type: ignore
 
 from .attempt import (
     aquire_attempt,
+    Attempt,
     create_next_attempt,
     get_last_attempt,
     grad_inc_gas_price
 )
-from .eth import Eth, is_replacement_underpriced, ReceiptTimeoutError
+from .config import CONFIRMATION_BLOCKS
+from .eth import (
+    Eth, is_replacement_underpriced, ReceiptTimeoutError
+)
 from .transaction import Tx, TxStatus
 from .txpool import TxPool
 
@@ -25,21 +30,55 @@ class Processor:
 
     def send(self, tx: Tx, nonce: int, gas_price: int) -> None:
         tx.chain_id = self.eth.chain_id
+        tx.source = self.wallet.address
         tx.gas_price = gas_price
         tx.nonce = nonce
         tx.gas = self.eth.calculate_gas(tx.eth_tx)
         # IVD TODO: Handle sgx wallet failling
         for _ in range(3):
+            logger.info(f'Signing transaction {tx.tx_id} {tx.eth_tx} ...')
+            signed = self.wallet.sign(tx.eth_tx)
             try:
-                signed = self.wallet.sign(tx.eth_tx)
                 # IVD TODO: Handle replacement underpriced error
+                logger.info(f'Sending transaction {tx}')
                 tx.tx_hash = self.eth.send_tx(signed)
-            except ValueError as err:
+            except Exception as err:
+                logger.info(f'Sending failed with error {err}')
                 if is_replacement_underpriced(err):
+                    logger.info(
+                        'Replacement gas price is too low. Trying to increase'
+                    )
                     gas_price = grad_inc_gas_price(gas_price)
+                else:
+                    raise err
+            else:
+                break
 
+        logger.info(f'Transaction {tx.tx_id} was sent successfully')
+        tx.status = TxStatus.SENT
         tx.sent_ts = int(time.time())
         tx.attempts += 1
+
+    def wait(self, tx: Tx, attempt: Attempt) -> Dict:
+        max_time = attempt.wait_time
+        receipt = None
+        if tx.tx_hash:
+            try:
+                receipt = self.eth.wait_for_receipt(
+                    tx_hash=tx.tx_hash,
+                    max_time=max_time
+                )
+            except ReceiptTimeoutError:
+                logger.info(f'{tx.tx_id} is not mined within {max_time}')
+                if attempt.is_last():
+                    tx.status = TxStatus.DROPPED
+                else:
+                    tx.status = TxStatus.TIMEOUT
+                return None
+        if receipt:
+            tx.set_as_completed(receipt)
+            self.eth.wait_for_blocks(CONFIRMATION_BLOCKS)
+            tx.status = TxStatus.CONFIRMED
 
     def handle(self, tx: Tx) -> None:
         # TODO: use tx.sent and fix mypy issues
@@ -48,8 +87,11 @@ class Processor:
             tx.set_as_completed(r)
             return
         else:
+            logger.info(f'Configuring dynamic params for {tx.tx_id}')
             avg_gp = self.eth.avg_gas_price
+            logger.info(f'Received avg gas pirce - {avg_gp}')
             nonce = self.eth.get_nonce(self.address)
+            logger.info(f'Received current nonce - {nonce}')
             prev_attempt = get_last_attempt()
             attempt = create_next_attempt(
                 nonce,
@@ -57,31 +99,19 @@ class Processor:
                 tx.tx_id,
                 prev_attempt
             )
-
-            with aquire_attempt(attempt, tx):
-                print('Aquiring attempt')
+            with aquire_attempt(attempt, tx) as attempt:
                 self.send(tx, attempt.nonce, attempt.gas_price)
-
-            max_time = attempt.wait_time
-
-            receipt = None
-            if tx.tx_hash:
-                try:
-                    receipt = self.eth.wait_for_receipt(
-                        tx_hash=tx.tx_hash,
-                        max_time=max_time
-                    )
-                except ReceiptTimeoutError:
-                    logger.info(f'{tx.tx_id} is not mined within {max_time}')
-                    if attempt.is_last():
-                        tx.status = TxStatus.NOT_SENT
-                    return
-        if receipt:
-            tx.set_as_completed(receipt)
+                self.wait(tx, attempt)
 
     def run(self) -> None:
         while True:
-            if self.pool.size > 0:
-                with self.pool.aquire_next() as tx:
-                    self.handle(tx)
+            try:
+                if self.pool.size > 0:
+                    with self.pool.aquire_next() as tx:
+                        logger.info(f'Received transaction {tx}')
+                        self.handle(tx)
+            except Exception:
+                logger.exception(
+                    'Fetching next tx failed'
+                )
             time.sleep(1)
