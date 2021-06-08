de0dd2a133ba6e120c95fc5ffb355e70e27de1aa
badrogger <ivan@skalelabs.com>
2021-06-08 11:46:42 +0300

    SKALE-3350 Increase base gas price coefficient

--- transaction_manager/eth.py
+++ transaction_manager/eth.py
@@ -31,7 +31,7 @@ from .resources import w3 as gw3
 from .config import (
     CONFIRMATION_BLOCKS,
     GAS_MULTIPLIER,
-    GAS_PRICE_INC_PERCENT,
+    AVG_GAS_PRICE_INC_PERCENT,
     MAX_WAITING_TIME
 )
 
@@ -74,7 +74,7 @@ class Eth:
 
     @property
     def avg_gas_price(self) -> int:
-        return self.w3.eth.gasPrice * (100 + GAS_PRICE_INC_PERCENT) // 100
+        return self.w3.eth.gasPrice * (100 + AVG_GAS_PRICE_INC_PERCENT) // 100
 
     def calculate_gas(self, tx: Dict, multiplier: Optional[float]) -> int:
         multiplier = multiplier or GAS_MULTIPLIER
