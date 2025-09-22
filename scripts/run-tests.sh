ce6e38604753e5451277c41cd7ff07731dc28b58
Dmytro <dmitry.tkk@gmail.com>
2025-09-22 17:39:34 +0100

    Remove extra logging in tests

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -7,7 +7,7 @@ export SKALE_DIR=${PROJECT_DIR}/tests/data-volumes/skale-dir
 
 export ETH_PRIVATE_KEY=$ETH_PRIVATE_KEY
 export PYTHONPATH=${PYTHONPATH}:$PROJECT_DIR
-py.test -o log_cli=true -vvv $PROJECT_DIR/tests/ \
+py.test $PROJECT_DIR/tests/ \
     --ignore $PROJECT_DIR/tests/docker_test.py \
         --cov-report term-missing \
             --cov $PROJECT_DIR/transaction_manager $@
